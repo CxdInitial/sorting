dd635c10ea9d015d8d4d1fb559d0f8d6018d7cbd
Cxd <2caoxingding@gmail.com>
2018-03-14 21:24:00 +0800

    Refactor insertion sorting

--- simple.py
+++ simple.py
@@ -2,17 +2,17 @@ def insert_sort(elements):
     """
     Use the simple insertion sort algorithm to sort the :param elements.
     :param elements: must be a sequence which has the function __get_item__ and __len__.
-    :return: the sorted elements
+    :return: the sorted elements in increasing order
     """
     if not len(elements) or len(elements) == 1:
         return elements
     for index in range(1, len(elements)):
         insert = elements[index]
-        for position in enumerate(elements[:index+1]):
-            if position[1] > insert:
+        for position in range(0, index+1):
+            if elements[position] > insert:
                 break
-        for i in range(index, position[0], -1):
+        for i in range(index, position, -1):
             elements[i] = elements[i-1]
-        elements[position[0]] = insert
+        elements[position] = insert
         print(insert, position, elements)
     return elements
