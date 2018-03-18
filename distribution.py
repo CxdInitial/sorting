e46bf2c8fb95d43c5086138d534e6fd82facf995
曹兴鼎 <mearsxxf@nefu.edu.cn>
2018-03-18 20:55:14 +0800

    Add bucket sorting(distribution sorting)

--- distribution.py
+++ distribution.py
@@ -27,3 +27,43 @@ def counting_sort(elements):
             length += 1
             count -= 1
     return elements
+
+
+def bucket_sort(elements, bucket_size=10):
+    """
+    Use the simple bucket sort algorithm to sort the :param elements.
+    :param bucket_size: the distribution buckets' size
+    :param elements: a integer sequence in which the function __get_item__ and __len__ were implemented()
+    :return: the sorted elements in increasing order
+    """
+    length = len(elements)
+    if not length or length == 1:
+        return elements
+    mini = maxi = elements[0]
+    for element in elements:
+        assert isinstance(element, int)
+        if element < mini:
+            mini = element
+        if element > maxi:
+            maxi = element
+    buckets_size = (maxi - mini + 1) // bucket_size
+    if (maxi - mini + 1) % bucket_size:
+        buckets_size += 1
+    buckets = []
+    for i in range(buckets_size):
+        buckets.append([None, None])
+    for element in elements:
+        index = element // bucket_size
+        ptr = buckets[index]
+        while ptr[1] and ptr[1][0] < element:
+            ptr = ptr[1]
+        element = [element, ptr[1]]
+        ptr[1] = element
+    length = 0
+    for bucket in buckets:
+        ptr = bucket[1]
+        while ptr:
+            elements[length] = ptr[0]
+            length += 1
+            ptr = ptr[1]
+    return elements
